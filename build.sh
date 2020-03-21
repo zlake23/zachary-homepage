785d618cd8356087c2b367d90956278223ba95ea
Zachary Lake <zlake23@gmail.com>
2020-03-20 17:52:00 -0700

    Removed bash build script

--- build.sh
+++
@@ -1,4 +0,0 @@
-cat templates/top.html content/middle-index.html templates/bottom.html > docs/index.html
-cat templates/top.html content/middle-blog.html templates/bottom.html > docs/blog.html
-cat templates/top.html content/middle-projects.html templates/bottom.html > docs/projects.html
-cat templates/top.html content/middle-contact.html templates/bottom.html > docs/contact.html
