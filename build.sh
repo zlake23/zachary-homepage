5bbda0ed3fbcf1a60fe017997f176db1cc6aa989
Zachary Lake <52633741+zlake23@users.noreply.github.com>
2020-03-31 20:26:32 -0700

    Added bash script file

---
+++ build.sh
@@ -0,0 +1,4 @@
+cat templates/top.html content/middle-index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/middle-blog.html templates/bottom.html > docs/blog.html
+cat templates/top.html content/middle-projects.html templates/bottom.html > docs/projects.html
+cat templates/top.html content/middle-contact.html templates/bottom.html > docs/contact.html
